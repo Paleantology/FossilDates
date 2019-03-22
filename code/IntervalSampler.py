42a906c2144572f3f843d0fd3be458ba3e3d7c9a
April Wright <wright.aprilm@gmail.com>
2019-03-22 10:52:39 -0500

    committing script changes with fewer function calls

--- code/IntervalSampler.py
+++ code/IntervalSampler.py
@@ -8,37 +8,29 @@ def taxon_sample(df, group_rank):
     samp = df.groupby(group_rank).apply(lambda x :x.iloc[random.choice(range(0,len(x)))])
     return(samp)
 
-def taxa_file(group_rank, name_column, min_column, max_column, new_taxon_column_name, new_age_column):
-    grouped = taxon_sample('Data/higher_taxa.csv', group_rank)
-    new_group = grouped[[name_column, min_column, max_column]]
-    tip_age = grouped[[name_column, max_column]]
-    tip_age.columns = [new_taxon_column_name, new_age_column]
-    new_group.columns = ['taxon', 'min_yr', 'max_yr']
-    return(new_group)
-    
-def final_ages():
+def final_ages(tax_samp):
     morph_temp = pd.read_csv("Data/taxa_template.tsv", sep='\t')
     mol_temp = pd.read_csv("Data/Mol/mol_df.tsv", sep='\t')
-    tip_age = pd.read_csv("Data/taxa_sample.tsv", sep='\t')
     morph_temp = morph_temp[['taxon', 'age']]
-    stacked = pd.concat([morph_temp, mol_temp, tip_age])
+    tax_samp = tax_samp[['SpecimenName', 'max_yr']]
+    tax_samp.columns = ['taxon', 'age']
+    stacked = pd.concat([morph_temp, mol_temp, tax_samp])
     final_tip_age = stacked.drop_duplicates('taxon')
     return(final_tip_age)
         
-def foss_int():
+def foss_int(tax_samp):
     morph_temp = pd.read_csv("Data/taxa_template.tsv", sep='\t')
-    sample = taxa_file('SubFamily', 'SpecimenName', 'min_yr', 'max_yr', 'taxon', 'age')
     morph_slim = morph_temp[['taxon', 'min_yr', 'max_yr']]
-    sample = sample[['taxon', 'min_yr', 'max_yr']]
+    tax_samp = tax_samp[['SpecimenName', 'min_yr', 'max_yr']]
+    tax_samp.columns = ['taxon', 'min_yr', 'max_yr']
     morph_slim = morph_slim[morph_slim.min_yr != 0.00]
-    stacked = pd.concat([sample, morph_slim])
+    stacked = pd.concat([tax_samp, morph_slim])
     return(stacked)
     
 if __name__ == '__main__':
-    tax_file = taxa_file('SubFamily', 'SpecimenName', 'min_yr', 'max_yr', 'taxon', 'age')
-    tax_file.to_csv("Data/taxa_sample.tsv", sep='\t', index =False)
-    final_tip_age = final_ages()
+    tax_samp = taxon_sample('Data/higher_taxa.csv', 'SubFamily')
+    final_tip_age = final_ages(tax_samp)
     final_tip_age.to_csv("Data/accessory/final_tip_age.tsv", sep = '\t', index=False)
-    stacked = foss_int()
+    stacked = foss_int(tax_samp)
     stacked.to_csv("Data/accessory/foss_int.tsv", sep='\t', index =False)
 
