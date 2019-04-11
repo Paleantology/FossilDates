db7a0fa27ebd403a8c6665be0d08a8c8b431b7ee
Kolbmann <wright.aprilm@gmail.com>
2019-04-11 10:07:02 -0500

    oldest fossils by Subfam

--- code/IntervalSampler.py
+++ code/IntervalSampler.py
@@ -1,31 +1,38 @@
+#########################################
+python IntervalSampler.py --df "../Data/higher_taxa.csv" --group_rank "SubFamily" --s_of "../Data/stacked_data_2.csv"
+#########################################
+
+
+
 import pandas as pd
 from numpy import random
-import argparse    
+import argparse 
+import dendropy
 
 def taxon_sample(df, group_rank):
     group_rank = group_rank
     df = pd.read_csv(df)
     df = df.dropna(axis=0)
-    samp = df.groupby(group_rank).apply(lambda x :x.iloc[random.choice(range(0,len(x)))])
-    return(samp)
+    tax_samp = df.groupby(group_rank).apply(lambda x :x.iloc[random.choice(range(0,len(x)))])
+    return(tax_samp)
 
-def final_ages(tax_samp, morph_temp, mol_temp):
-    morph_temp = pd.read_csv(morph_temp, sep='\t')
-    mol_temp = pd.read_csv(mol_temp, sep='\t')
-    morph_temp = morph_temp[['taxon', 'age']]
+def final_ages(tax_samp):
+    #morph_temp = dendropy.StandardCharacterMatrix.get(path=morph_temp, schema="nexus")
+    #mol_temp = dendropy.StandardCharacterMatrix.get(path=mol_temp, schema="nexus")
+    #tax_samp = tax_samp[['taxon', 'age']]
     tax_samp = tax_samp[['SpecimenName', 'max_yr']]
     tax_samp.columns = ['taxon', 'age']
-    stacked = pd.concat([morph_temp, mol_temp, tax_samp])
-    final_tip_age = stacked.drop_duplicates('taxon')
+    #stacked = pd.concat([morph_temp, mol_temp, tax_samp])
+    final_tip_age = tax_samp.drop_duplicates('taxon')
     return(final_tip_age)
         
-def foss_int(tax_samp, morph_temp):
-    morph_temp = pd.read_csv(morph_temp, sep='\t')
-    morph_slim = morph_temp[['taxon', 'min_yr', 'max_yr']]
-    tax_samp = tax_samp[['SpecimenName', 'min_yr', 'max_yr']]
-    tax_samp.columns = ['taxon', 'min_yr', 'max_yr']
-    morph_slim = morph_slim[morph_slim.min_yr != 0.00]
-    stacked = pd.concat([tax_samp, morph_slim])
+def foss_int(tax_samp, final_tip_age):
+    #morph_temp = pd.read_csv(morph_temp, sep='\t')
+    morph_slim = final_tip_age[['taxon', 'age']]
+    #tax_samp = tax_samp[['SpecimenName', 'min_yr', 'max_yr']]
+    #tax_samp.columns = ['taxon', 'min_yr', 'max_yr']
+    morph_slim = morph_slim[morph_slim.age != 0.00]
+    stacked = pd.concat([final_tip_age, morph_slim])
     return(stacked)
 
 
@@ -34,8 +41,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--df", help = "TSV Containing data for sample taxa")
-    parser.add_argument("--morph", help = "TSV Containing data for morph characters")
-    parser.add_argument("--mol", help = "TSV Containing data for mol characters")
+    #parser.add_argument("--morph", help = "TSV Containing data for morph characters")
+    #parser.add_argument("--mol", help = "TSV Containing data for mol characters")
     parser.add_argument("--group_rank", help = "rank")
     parser.add_argument("--outfile", help = "The file path you wish to write the data to")
     parser.add_argument("--fta_of", help = "The file path you wish to write the final_tip_age data to")
@@ -43,10 +50,10 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    if args.morph:
-        morph_temp = args.morph
-    if args.mol:
-        mol_temp = args.mol
+    #if args.morph:
+      #  morph_temp = args.morph
+    #if args.mol:
+     #   mol_temp = args.mol
     if args.group_rank:
         group_rank = args.group_rank
     if args.df:
@@ -57,9 +64,9 @@ if __name__ == '__main__':
         stacked_of = args.s_of
 
     tax_samp = taxon_sample(df, group_rank)
-    final_tip_age = final_ages(tax_samp, morph_temp, mol_temp)
-    final_tip_age.to_csv(final_tip_age_of, sep = '\t', index=False)
-    stacked = foss_int(tax_samp, morph_temp)
+    final_tip_age = final_ages(tax_samp)
+    final_tip_age.to_csv(args.fta_of, sep = '\t', index=False)
+    stacked = foss_int(tax_samp, final_tip_age)
     stacked.to_csv(stacked_of, sep='\t', index =False)
     
 
